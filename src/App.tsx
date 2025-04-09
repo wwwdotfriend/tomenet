4ce8bc1132887585964353c6481d818e1f5562da
luki <me@lukasparedes.com>
2025-04-09 08:13:21 -0400

    feat: installed & switched to tailwindcss, added homepage bg and logo, update .gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import TomenetLogo from './components/TomenetLogo';
+import backgroundVideo from "/HomepageBackground.webm";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='relative w-screen h-screen overflow-hidden z-0'>
+      <video className="fixed object-cover" autoPlay loop muted>
+        <source src={backgroundVideo} type='video/webm' />
+      </video>
+      
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[120%] tomenetShadow">
+        <TomenetLogo />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/TomenetLogo.tsx
@@ -0,0 +1,7 @@
+import tomenetLogo from "/TomenetLogo.svg"
+
+function TomenetLogo() {
+    return <img src={tomenetLogo} alt="TomeNet Logo" />;
+}
+
+export default TomenetLogo;
