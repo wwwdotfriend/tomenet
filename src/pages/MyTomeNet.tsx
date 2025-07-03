3edf10b6814c550ae881d5678980e651eaa34280
luki <Lukas>
2025-07-03 06:30:37 -0400

    refactor: prettified (+tailwind prettier)

--- src/pages/MyTomeNet.tsx
+++ src/pages/MyTomeNet.tsx
@@ -1,13 +1,15 @@
-import Sidebar from '../components/Sidebar';
+import Sidebar from "../components/Sidebar";
 
 export default function MyTomeNet() {
-    return <div className="bg-[#FBF5F1] w-screen h-screen">
-        <div>
+  return (
+    <div className="h-screen w-screen bg-[#FBF5F1]">
+      <div>
         <Sidebar />
-        </div>
-        
-        <div className="pt-15 flex justify-center">
-            <h1>My TomeNet</h1>
-        </div>
+      </div>
+
+      <div className="flex justify-center pt-15">
+        <h1>My TomeNet</h1>
+      </div>
     </div>
+  );
 }
--- src/pages/SocialHub.tsx
+++ src/pages/SocialHub.tsx
@@ -1,13 +1,15 @@
-import Sidebar from '../components/Sidebar';
+import Sidebar from "../components/Sidebar";
 
 export default function SocialHub() {
-    return <div className="bg-[#FBF5F1] w-screen h-screen">
-        <div>
+  return (
+    <div className="h-screen w-screen bg-[#FBF5F1]">
+      <div>
         <Sidebar />
-        </div>
-        
-        <div className="pt-15 flex justify-center">
-            <h1>Social Hub</h1>
-        </div>
+      </div>
+
+      <div className="flex justify-center pt-15">
+        <h1>Social Hub</h1>
+      </div>
     </div>
+  );
 }
