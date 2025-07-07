9094fb86c88638710a7658b883e368834f0f6ebe
luki <Lukas>
2025-07-07 15:38:25 -0400

    refac: added fallback photourl for PostInput

--- src/components/ashTag/PostInput.tsx
+++ src/components/ashTag/PostInput.tsx
@@ -82,10 +82,14 @@ export default function PostInput({ insideModal }: PostInputProps) {
     <div className="flex space-x-5 border-b border-[#696765] px-3 pt-5 pb-4">
       <img
         className="z-10 h-11 w-11 rounded-full object-cover"
-        src={photoURL}
+        src={photoURL || "https://i.imgur.com/zKbL9ih.png"}
         width={44}
         height={44}
         alt="Profile Picture"
+        onError={(e) => {
+          const target = e.target as HTMLImageElement;
+          target.src = "https://i.imgur.com/zKbL9ih.png";
+        }}
       />
 
       <div className="w-full text-white">
@@ -98,11 +102,11 @@ export default function PostInput({ insideModal }: PostInputProps) {
 
         <div className="flex items-center justify-between pt-5">
           <div className="flex space-x-1.5">
-            <PhotoIcon className="h-[22px] w-[22px] text-amber-700 cursor-not-allowed" />
-            <ChartBarIcon className="h-[22px] w-[22px] text-amber-700 cursor-not-allowed" />
-            <FaceSmileIcon className="h-[22px] w-[22px] text-amber-700 cursor-not-allowed" />
-            <CalendarIcon className="h-[22px] w-[22px] text-amber-700 cursor-not-allowed" />
-            <MapPinIcon className="h-[22px] w-[22px] text-amber-700 cursor-not-allowed" />
+            <PhotoIcon className="h-[22px] w-[22px] cursor-not-allowed text-amber-700" />
+            <ChartBarIcon className="h-[22px] w-[22px] cursor-not-allowed text-amber-700" />
+            <FaceSmileIcon className="h-[22px] w-[22px] cursor-not-allowed text-amber-700" />
+            <CalendarIcon className="h-[22px] w-[22px] cursor-not-allowed text-amber-700" />
+            <MapPinIcon className="h-[22px] w-[22px] cursor-not-allowed text-amber-700" />
           </div>
 
           <button
